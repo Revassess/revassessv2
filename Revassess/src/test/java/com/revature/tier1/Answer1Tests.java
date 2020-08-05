f4c49a95c92ba226a58d3c75d37cfddb4a0ca502
Robert Connell <47120126+connellrobert@users.noreply.github.com>
2020-08-04 22:26:42 -0400

    I'm kicking my self over not seeing this earlier

--- Revassess/src/test/java/com/revature/tier1/Answer1Tests.java
+++ Revassess/src/test/java/com/revature/tier1/Answer1Tests.java
@@ -18,9 +18,9 @@ public class Answer1Tests {
     @Test
     public void test1(){
         assertFalse(CompareStrings.compareStrings("s1", "s2"));
-        assertTrue(CompareStrings.compareStrings("hello", "hello"));
-        assertTrue(CompareStrings.compareStrings("longhorn", "longhorn"));
-        assertTrue(CompareStrings.compareStrings("gentleman", "gentleman"));
+        assertTrue(CompareStrings.compareStrings("hello", new String("hello")));
+        assertTrue(CompareStrings.compareStrings("longhorn", new String("longhorn")));
+        assertTrue(CompareStrings.compareStrings("gentleman", new String("gentleman")));
         assertFalse(CompareStrings.compareStrings("jekyll", "hyde"));
         assertFalse(CompareStrings.compareStrings("giraffes", "real"));
         addPoints(10);
